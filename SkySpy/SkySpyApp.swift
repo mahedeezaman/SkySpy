45a536fae78cdcea5e6aeffedfc92d8b8c3b5d94
Kobiraj <mmoon@laurentian.ca>
2024-02-14 20:45:20 -0500

    Initial commit

---
+++ SkySpy/SkySpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkySpyApp.swift
+//  SkySpy
+//
+//  Created by Mahedee Zaman on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkySpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
